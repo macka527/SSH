c1a7dbeca37b4f0749409341f7e003f7b2b9de9c
macka527 <60435246+macka527@users.noreply.github.com>
2023-09-20 11:24:45 +0500

    Update modul.py

--- modul.py
+++ modul.py
@@ -1,75 +1,59 @@
 import paramiko
-import time
 from time import monotonic as _time
+import time
 
-class My_Telnetlib():
-    """
-    Заходит на SSH сервер по закрытому ключу и выполняет подключения Telnet с этого сервера
-    """
+host = "10.128.0.126"
+username = "huaweitftp"
+password = "Qwerty123Qwerty"
+
+class SOM():
     def __init__(
         self,        
-        ip,
-        host_ssh,
-        user_ssh,
-        your_ip,
-        ):
-        self.ip = ip, # host for telnet
-        self.host_ssh = host_ssh
-        self.user_ssh = user_ssh
-        self.your_ip = your_ip
+        host,
+        user,
+        password,
+        ):      
+        self.host = host
+        self.user = user
+        self.password = password
         self.client = paramiko.SSHClient()
-        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())       
-        self.client.connect(hostname=self.host_ssh, username=self.user_ssh)
-        self.ssh = self.client.invoke_shell()
-        self.ssh_telnet_connect()
-    
-    def ssh_telnet_connect(self, timeout=10):
-        self.ssh.send(f"telnet {self.ip[0]}\n")
-        deadline = _time() + timeout
-        a = '-'        
-        while a:            
-            if 'Connected to' in a:
-                a = True
-                break            
-            elif ('onnection refused' in a) or ('onnection timed out' in a):                
-                del a
-                break
-            else:
-                sad = self.ssh.recv_ready()
-                if sad is True:
-                    a = self.ssh.recv(3000).decode('utf-8')
-                elif sad is False:
-                    pass                  
-            timeout = deadline - _time()
-            if timeout < 0:
-                del a
-                break
-        return a
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(hostname=self.host, username=self.user, password=self.password)
+        self.ssh = self.client.invoke_shell()    
 
-    def read_until_ssh(self, param1, param2='-None-', timeout=None):       
+    def read_until_ssh(self, param1, param2='-None-', timeout=None):      
+        # all_b список для записи всех собранных байт
         all_b = []
-        b = self.ssh.recv(3000).decode('utf-8')        
-        if self.your_ip in b:
-            b = '-'
-        else:
-            all_b.append(b)        
+        # Проверяет создан ли таймер, если создан монтирует точку отсчета deadline
         if timeout is not None:
-            deadline = _time() + timeout            
-        while b:
-            if (param1 in b) or (param2 in b) :                
-                    break                
-            else:
-                sad = self.ssh.recv_ready()                
-                if sad is True:
-                    b = self.ssh.recv(3000).decode('utf-8')                                  
-                    all_b.append(b)
-                elif sad is False:
-                    pass
-            if timeout is not None:
-                timeout = deadline - _time()
-                if timeout < 0:
-                    all_b.append('timeout')
-                    break                 
+            deadline = _time() + timeout
+
+        while True:
+            time.sleep(0.1)
+            # проверяем есть ли байты готовые к получению
+            sad = self.ssh.recv_ready()
+            if sad is True:
+                # если есть байты готовые к получению, собираем их и записываем в all_b
+                b = self.ssh.recv(3000).decode('utf-8')
+                all_b.append(b)
+
+                # проверяем найден ли хотя бы один из параметров
+                if (param1 in b) or (param2 in b):                
+                    break
+                # проверяем таймер
+                if timeout is not None:
+                    timeout = deadline - _time()
+                    if timeout < 0:
+                        all_b.append('timeout')
+                        break
+            elif sad is False:
+                # если нет байт готовых к получению, проверяем таймер
+                # проверяем таймер
+                if timeout is not None:
+                    timeout = deadline - _time()
+                    if timeout < 0:
+                        all_b.append('timeout')
+                        break
         return all_b
 
     def write_ssh(self, text):
@@ -79,18 +63,16 @@ class My_Telnetlib():
         self.client.close()
 
     def recv_ssh(self):
-        time.sleep(0.2)
+        time.sleep(0.1)
         b = self.ssh.recv(3000).decode('utf-8')
         return b
 
 
+if __name__ == '__main__':
+    ssh1 = SOM(host, username, password)
+    ssh1.recv_ssh()
 
-
-
-    
-if __name__=="__main__":    
-    pass
-else:
-	pass
-
-    
+    ssh1.write_ssh('display saved-configuration time\n')
+    text = ssh1.read_until_ssh('Info:Current configuration has not been saved automatically since system booted or slave switchover', timeout=10)
+    print(text)
+    ssh1.close_ssh()
